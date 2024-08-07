f879aa5ce0397b8808db193b57b96ca388fb6cb6
Rikhil09 <monditokarikhil09@gmail.com>
2024-08-07 15:33:30 +0530

    made changes in product section 1

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Product from '../Components/Flow3/Product'
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <Product />
+    </div>
+  )
+}
+
+export default ServicesPage
