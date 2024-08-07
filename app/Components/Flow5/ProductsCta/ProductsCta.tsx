4d434b2b928154b1f6ae47f3e798e37d8bca3f82
Himanshu4252 <himanshuhddudhpuri4252@gmail.com>
2024-08-07 23:22:15 +0530

    added section 1 and 2 in about page flow 5

--- app/Components/Flow5/ProductsCta/ProductsCta.tsx
+++ app/Components/Flow5/ProductsCta/ProductsCta.tsx
@@ -7,7 +7,7 @@ export const ProductsCta = () => {
     <div className='flex flex-col justify-center items-center w-screen h-screen bg-black '>
         <div className="flex  flex-col justify-center items-center  max-w-[1024px] text-white p-4">
 
-        <h3 className="text-5xl text-center font-semibold mb-4">
+        <h3 className="text-5xl text-center font-semibold mb-2 mt-8">
         Ready to Transform Your Digital Presence?
         </h3>
         <p className="text-xl text-center font-base my-5">
@@ -20,7 +20,7 @@ export const ProductsCta = () => {
         <button className="bg-orange-500 text-xl text-white px-6 py-2 rounded-full">Get Started</button>
         <button className="bg-transparent text-xl border border-orange-500   text-white px-6 py-6 rounded-full">Free Consultation</button>
     </div>
-    <Image className='mt-12' src={ZikraByteLogo} alt="ZikraByte Logp" />
+    <Image className='mt-4' src={ZikraByteLogo} alt="ZikraByte Logp" />
         </div>
     </div>
   )
